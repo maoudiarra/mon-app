a0bb42ac6ae656ba8427531386949a7aa5f26a19
Maoud Iarra <diarramaou@gmail.com>
2026-05-05 16:26:05 +0200

    Initial commit - pipeline as code

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello EPSI 🚀</h1>
+      <p>Projet Pipeline as Code</p>
+      <p>Nom : Ton prénom</p>
+    </main>
+  );
+}
